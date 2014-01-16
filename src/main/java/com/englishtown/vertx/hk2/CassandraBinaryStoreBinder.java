cc94f4d545ecd7f9a6fcdd47d7a75fbd98cb9b38
Adrian Gonzalez <adrian.gonzalez@ef.com>
2014-01-15 19:12:24 -0500

    Added explicit hk2 CassandraBinaryStoreBinder

--- src/main/java/com/englishtown/vertx/hk2/CassandraBinaryStoreBinder.java
+++ src/main/java/com/englishtown/vertx/hk2/CassandraBinaryStoreBinder.java
@@ -1,11 +1,12 @@
 package com.englishtown.vertx.hk2;
 
+import com.datastax.driver.core.Cluster;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
 /**
  *
  */
-public class BootstrapBinder extends AbstractBinder {
+public class CassandraBinaryStoreBinder extends AbstractBinder {
 
     /**
      * Implement to provide binding definitions using the exposed binding
@@ -14,7 +15,9 @@ public class BootstrapBinder extends AbstractBinder {
     @Override
     protected void configure() {
 
-        install(new CassandraBinaryStoreBinder());
+        install(new MetricsBinder());
+
+        bind(Cluster.Builder.class).to(Cluster.Builder.class);
 
     }
 }
